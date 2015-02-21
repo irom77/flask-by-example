43ee867af1293306bae7d40656a631d38b94981b
Michael Herman <hermanmu@gmail.com>
2015-02-21 10:14:17 -0700

    part1

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 
 @app.route('/')
@@ -10,7 +13,6 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
-
 
 if __name__ == '__main__':
     app.run()
